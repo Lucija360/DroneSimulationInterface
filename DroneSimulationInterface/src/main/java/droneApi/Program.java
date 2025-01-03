9ea6c1a309de9c8a119aee7dea2355e49c3112ff
HP <HP@DESKTOP-KQ5QGM2>
2025-01-03 09:58:08 +0100

    DroneApi.gui added to project with 4 classes. Program.java updated.

--- DroneSimulationInterface/src/main/java/droneApi/Program.java
+++ DroneSimulationInterface/src/main/java/droneApi/Program.java
@@ -1,3 +1,4 @@
+/*
 package droneApi;
 
 import org.springframework.boot.SpringApplication;
@@ -16,7 +17,7 @@ public class Program implements WebMvcConfigurer {
     	 /**
          * Main method that starts the Spring Boot application.
          * @param args command-line arguments.
-         */
+        //deleted
         SpringApplication.run(Program.class, args);
     }
 
@@ -24,8 +25,67 @@ public class Program implements WebMvcConfigurer {
      * Configures the default view controller.
      * Maps the root URL "/" to the "index" view.
      * This expects an "index.html" file in the "resources/templates" directory.
-     */
+     //deleted
     //TODO: You can create corresponding HTML templates in the resources/templates folder for UI elements
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+    /**
+     * Registers additional view controllers.
+     * Maps the "/drones" URL to the "drones" view.
+     * This expects a "drones.html" file in the "resources/templates" directory.
+     * @return WebMvcConfigurer instance with custom view controllers.
+     //deleted
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/drones").setViewName("drones");
+            }
+        };
+    }
+}
+
+*/
+
+// UPDATED PROGRAM.java
+
+package droneApi;
+
+import droneApi.Gui.MainMenu;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableWebMvc
+public class Program implements WebMvcConfigurer {
+
+    public static void main(String[] args) {
+        // Start the Spring Boot application
+        SpringApplication.run(Program.class, args);
+
+        // Launch the Swing GUI in a separate thread
+
+        javax.swing.SwingUtilities.invokeLater(() -> {
+        	MainMenu mainMenu = new MainMenu(); //MainMenu class will be used here
+        	mainMenu.setVisible(true);
+     });
+
+        System.out.println("Spring Boot Application and Swing GUI are running...");
+    }
+
+    /**
+     * Configures the default view controller.
+     * Maps the root URL "/" to the "index" view.
+     * This expects an "index.html" file in the "resources/templates" directory.
+     */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
@@ -47,3 +107,4 @@ public class Program implements WebMvcConfigurer {
         };
     }
 }
+
